c9d4d7e11c0235beb08da85957a79891a0730fea
Vivien Laquay <vivien.laquay@gmail.com>
2024-04-12 10:49:53 +0200

    header soso

--- my_first_app/src/components/Wrapper/Header.js
+++ my_first_app/src/components/Wrapper/Header.js
@@ -1,32 +1,34 @@
-import React from "react";
 import { Link } from "react-router-dom";
-import '../../assets/styles/Header.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCat } from "@fortawesome/free-solid-svg-icons";
-
-
 
 const Header = () => {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <Link className="navbar-brand" to={"/"}>
+                    Accueil
+                </Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div className="navbar-nav">
-                        <Link className="nav-link" to={"/"}>
-                            <FontAwesomeIcon icon={faCat} />
-                        </Link>
-                        <Link className="nav-link" to={"/contact"}>
-                            Contact
-                        </Link>
-                    </div>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link className="nav-link active" aria-current="page" to={"/business"}>Notre Entreprise</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to={"/service"}>Nos Prestations</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to={"/blog"}>L'Actualité</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to={"/contact"}>Nous Contacter</Link>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </nav>
     );
-};
+}
 
 export default Header;
